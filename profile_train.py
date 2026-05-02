dc633e26994901fd95e9283c3ce8d3414a811a7d
Vaishak Menon <vaishakkmenon@gmail.com>
2026-05-01 18:30:29 -0600

    try batches of 16 again

--- profile_train.py
+++ profile_train.py
@@ -6,8 +6,8 @@ import numpy as np
 from model.model import ShakGPT
 from model.config import ModelConfig
 
-GRAD_ACCUM_STEPS = 4
-BATCH_SIZE = 8
+GRAD_ACCUM_STEPS = 2
+BATCH_SIZE = 16
 SEQ_LEN = 2048
 
 class ShakGPTDataModule():
@@ -45,7 +45,7 @@ def main():
     dtype = torch.bfloat16
     
     model = ShakGPT(ModelConfig()).to(device)
-    model = torch.compile(model, mode="reduce-overhead")
+    model = torch.compile(model)
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=0.1, betas=(0.9, 0.95), fused=True)
     train_loader = ShakGPTDataModule(batch_size=BATCH_SIZE, data_path="data/processed/train.bin")
     
